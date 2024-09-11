06e3b5f6f6130727870e7c7db4da9705ba24c034
Stancu2001 <fstancu346@gmail.com>
2024-09-11 15:51:23 +0300

    scos debugger

--- frontend-src/src/app/shared/services/auth.service.ts
+++ frontend-src/src/app/shared/services/auth.service.ts
@@ -13,8 +13,6 @@ export class AuthService {
 
   login(email: string, password: string) {
     const hashedPassword = CryptoJS.SHA256(password).toString(CryptoJS.enc.Hex);
-  console.log(hashedPassword);
-  debugger
     return this._httpClient.post(`${environment.apiBaseUri}/api/auth/login`, {
       email,
       password: hashedPassword,
