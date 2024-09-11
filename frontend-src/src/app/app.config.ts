0be24c784feb42e7728c58dbd2666c789d9f4c0d
ALEX\picua <picuandreia2001@gmail.com>
2024-09-11 16:02:08 +0300

    Add interceptor

--- frontend-src/src/app/app.config.ts
+++ frontend-src/src/app/app.config.ts
@@ -3,16 +3,17 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideAnimations } from '@angular/platform-browser/animations';
-import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { ConfirmationService, MessageService } from 'primeng/api';
 import { DialogService } from 'primeng/dynamicdialog';
+import { authInterceptor } from './core/auth.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     provideAnimations(),
-    provideHttpClient(),
+    provideHttpClient(withInterceptors([authInterceptor])),
 
     ConfirmationService,
     DialogService,
